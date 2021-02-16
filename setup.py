64cf06e45212841dc7905ccdefa2076d9dc8e4cf
jochen <jochen.zoellner@planet-ai.de>
2021-02-16 15:54:47 +0100

    update setup.py, README.md, requirements.txt

--- setup.py
+++ setup.py
@@ -16,16 +16,19 @@
 # tfaip. If not, see http://www.gnu.org/licenses/.
 # ==============================================================================
 from setuptools import setup, find_packages
+import os
 from tfaip import __version__
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
+
 
 setup(
     name='tfaip',
     version=__version__,
     packages=find_packages(exclude=['test/*']),
     license='GPL-v3.0',
-    long_description=open("README.md").read(),
+    long_description=open(os.path.join(this_dir, "README.md")).read(),
     long_description_content_type="text/markdown",
-    include_package_data=True,
     author="Planet AI GmbH",
     author_email="admin@planet-ai.de",
     url="https://github.com/Planet-AI-GmbH/tf2_aip_base",
@@ -40,8 +43,7 @@ setup(
         ],
     },
     python_requires='>=3.7',
-    install_requires=open("requirements.txt").read().split('\n'),
-    tests_requires=[],
+    install_requires=open(os.path.join(this_dir, "requirements.txt")).read().split('\n'),
     keywords=['machine learning', 'tensorflow', 'framework'],
-    data_files=[('', ["requirements.txt"])],
+    data_files=[('', [os.path.join(this_dir, "requirements.txt")])],
 )
